bb5e70622c00870dd0a5d86075d1d23999300271
Steven Gregory <steven@silverhammertech.com>
2020-06-16 18:49:44 -0500

    exercise1 commit

---
+++ exercise1jl1.go
@@ -0,0 +1,12 @@
+package exer1jl1
+import "fmt"
+
+func exer1jl1() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("%v\t%v\t%v", x, y, z)
+
+
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import
+
+func main() {
+	fmt.Println("Hello my subjects")
+	foo()
+	exer1jl1()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
